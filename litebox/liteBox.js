065062ad0fea53c4d9d377794e31b0548899b694
Alexsandro Pereira <apssouza22@gmail.com>
2012-01-12 12:52:30 -0200

    add novas validacoes: cep, url, cnpj, number, digits

--- litebox/liteBox.js
+++ litebox/liteBox.js
@@ -3,9 +3,9 @@
 var Popup = function(elPopup)
 {
 	//objetos Domm 
-	var DOOM_popup 	= $(elPopup) || $('.popUp');
-	var DOOM_btCloseBox = $('.close');
-	var DOOM_mask 		=  createMask();
+	var DOM_popup 	= $(elPopup) || $('.popUp');
+	var DOM_btCloseBox = $('.close');
+	var DOM_mask 		=  createMask();
 
 	applyListners();//LISTENER DOS BOTÕES
 	
@@ -13,11 +13,11 @@ var Popup = function(elPopup)
 	var returnPublic = {};
 
 	function positionCenterPopUP(){
-		var width = DOOM_popup.width();
-		var height = DOOM_popup.height();
+		var width = DOM_popup.width();
+		var height = DOM_popup.height();
 		var posTop = Math.floor(height/2);
 		var posLeft = Math.floor(width/2);
-		DOOM_popup.css({
+		DOM_popup.css({
 			marginLeft: -posLeft,
 			marginTop: -posTop
 		});
@@ -44,18 +44,18 @@ var Popup = function(elPopup)
 	
 	function applyListners()
 	{		
-		DOOM_btCloseBox.live('click',closeBox);
+		DOM_btCloseBox.live('click',closeBox);
 	}
 
 	function closeBox(){
-		DOOM_popup .fadeOut();
-		DOOM_mask.fadeOut();
+		DOM_popup .fadeOut();
+		DOM_mask.fadeOut();
 	}
 
 	 returnPublic.showPopup = function ()
 	 {
-		DOOM_popup.fadeIn()
-		DOOM_mask.fadeTo("slow",0.7);
+		DOM_popup.fadeIn()
+		DOM_mask.fadeTo("slow",0.7);
 		positionCenterPopUP(); //CENTRALIZA POPUP
 		return false;
 	}
@@ -71,11 +71,11 @@ var Popup = function(elPopup)
 	returnPublic.addTemplateDefault = function(){
 		var templateDefault = $('<div class="popUpDefault hide"><a class="button closePopup close ir">X</a><div class="popUpContent clearfix"></div></div>');
 		templateDefault.find('.popUpContent')
-			.append(DOOM_popup);
+			.append(DOM_popup);
 		
-		DOOM_popup.removeClass('hide');//agora o template que fica escondido
-		DOOM_popup = templateDefault;
-		$('body').append(DOOM_popup);
+		DOM_popup.removeClass('hide');//agora o template que fica escondido
+		DOM_popup = templateDefault;
+		$('body').append(DOM_popup);
 
 	}
 
