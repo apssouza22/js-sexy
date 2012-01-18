3bbd0e0c48c975f1f0606db72935b538811d220a
Alexsandro Pereira <apssouza22@gmail.com>
2012-01-18 16:31:09 -0200

    add o metodo equal, para validar campos iguais e agora estamos retornando o metodo executeValidat que permite executar a validacao quando quiser

--- validate/validaForm.js
+++ validate/validaForm.js
@@ -22,6 +22,7 @@ $.fn.configForm = function (options)
 	var self = $(this),
 		fields = self.find("input, textarea,select"),
 		args = arguments,
+		currentEvent,
 
 		defaults = {
 			addFieldError: function (field) {
@@ -41,15 +42,83 @@ $.fn.configForm = function (options)
 	var listMethods = {
 			'required' : function(value,field)
 			{
-				if(field.attr('type') == 'checkbox'){
+				if(field.attr('type') == 'checkbox') {
 					return field.is(':checked');
 				}
+				
+				if (field.attr('type') == 'radio') {
+					var itens = self.find("[name=" + field.attr("name") + "]");
+					return itens.filter(":checked").length != 0;
+				}
+
 				return value != "";
 			},
 
 			'email' : function(value)
 			{
 				return /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i.test(value);
+			},
+			
+			'number' : function(value)
+			{
+				return /^-?(?:\d+|\d{1,3}(?:,\d{3})+)(?:\.\d+)?$/.test(value);
+			},
+			
+			'digits' : function(value) 
+			{
+				return  /^\d+$/.test(value);
+			},
+			
+			'cep' : function(value) 
+			{
+				return  /^\d+$/.test(value) && value.length == 8;
+			},
+			
+			'equal' : function (value, field) {
+				
+				if (!currentEvent) return true;
+
+				var value = field.val(),
+					fields = self.find("[name=" + field.attr("name") + "]"),
+					result;
+
+				fields.each(function () {
+					result = value === $(this).val();
+					return result;
+				});
+
+				return result;
+			},
+			
+			// based on http://en.wikipedia.org/wiki/Luhn
+			'creditcard' : function(value, element) 
+			{
+				// accept only spaces, digits and dashes
+				if (/[^0-9 -]+/.test(value))
+					return false;
+				var nCheck = 0,
+					nDigit = 0,
+					bEven = false;
+	
+				value = value.replace(/\D/g, "");
+	
+				for (var n = value.length - 1; n >= 0; n--) {
+					var cDigit = value.charAt(n);
+					var nDigit = parseInt(cDigit, 10);
+					if (bEven) {
+						if ((nDigit *= 2) > 9)
+							nDigit -= 9;
+					}
+					nCheck += nDigit;
+					bEven = !bEven;
+				}
+	
+				return (nCheck % 10) == 0;
+			},
+			
+			url: function(value, element) {
+				// contributed by Scott Gonzalez: http://projects.scottsplayground.com/iri/
+				return  /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(value);
 			},
 
 			'cpf' : function (value)
@@ -133,12 +202,14 @@ $.fn.configForm = function (options)
 	//EVENTOS QUE DISPARA A VALIDA��O
 
 	//EVENTO SUBMIT
-	self.submit(function ()
+	self.submit(executeValidate);
+	
+	function executeValidate (event)
 	{
 		var currentField;
 		var resultField;
 		var finalResult = true;
-
+		currentEvent = event || false;
 		// check each field
 		fields.each(function ()
 		{
@@ -161,11 +232,12 @@ $.fn.configForm = function (options)
 			}
 		}
 		return finalResult;
-	});
+	}
 
 	if(defaults.onFocus) {
-		fields.focusout(function ()
+		fields.focusout(function (event)
 		{
+			currentEvent = false;
 			if(validateField($(this))) {
 				defaults.removeFieldError($(this));
 			} else {
@@ -173,5 +245,8 @@ $.fn.configForm = function (options)
 			}
 		});
 	}
-
+	
+	return {
+		executeValidate: executeValidate
+	};
 }
